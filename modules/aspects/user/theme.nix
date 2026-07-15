a94c78f2a26ecfb459d6bae84833e35a0fdc77d8
Oery <oery.dev@gmail.com>
2026-07-15 13:15:54 +0200

    ref: made explicit nix setting

--- modules/aspects/user/theme.nix
+++ modules/aspects/user/theme.nix
@@ -8,6 +8,7 @@
       { pkgs, config, ... }:
       {
         home.pointerCursor = {
+          enable = true;
           gtk.enable = true;
           x11.enable = true;
           package = pkgs.bibata-cursors;
