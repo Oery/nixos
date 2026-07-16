74f9cd0cdf45ff3347437dc7d612b7e61f22d566
Oery <oery.dev@gmail.com>
2026-07-16 11:17:19 +0200

    mime: set mpv as default video player

--- modules/aspects/user/services.nix
+++ modules/aspects/user/services.nix
@@ -65,7 +65,27 @@
         enable = true;
 
         mime.enable = true;
-        mimeApps.enable = true;
+        mimeApps = {
+          enable = true;
+          defaultApplications = {
+            "video/mp4" = "mpv.desktop";
+            "video/mkv" = "mpv.desktop";
+            "video/webm" = "mpv.desktop";
+            "video/avi" = "mpv.desktop";
+            "video/quicktime" = "mpv.desktop";
+            "video/x-msvideo" = "mpv.desktop";
+            "video/x-matroska" = "mpv.desktop";
+            "video/ogg" = "mpv.desktop";
+            "video/x-flv" = "mpv.desktop";
+            "video/x-ms-wmv" = "mpv.desktop";
+            "video/mpeg" = "mpv.desktop";
+            "video/3gpp" = "mpv.desktop";
+            "video/x-ms-asf" = "mpv.desktop";
+            "application/vnd.apple.mpegurl" = "mpv.desktop";
+            "video/x-m4v" = "mpv.desktop";
+            "video/x-ogm+ogg" = "mpv.desktop";
+          };
+        };
 
         portal = {
           enable = true;
