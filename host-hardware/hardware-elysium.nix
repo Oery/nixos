2d065961059f4e78213fad968d0be09bab57487f
Oery <oery.dev@gmail.com>
2026-08-30 14:15:23 +0200

    hosts: reset elysium

--- host-hardware/hardware-elysium.nix
+++ host-hardware/hardware-elysium.nix
@@ -10,12 +10,12 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d8869aab-0ad7-4666-8406-44a798658034";
+    { device = "/dev/disk/by-uuid/f5848461-3cb7-421b-8575-b5ddc724e293";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/19A0-1812";
+    { device = "/dev/disk/by-uuid/4607-1E14";
       fsType = "vfat";
       options = [ "fmask=0022" "dmask=0022" ];
     };
