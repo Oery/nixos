a2167a3c0aa735a6e4cda351af668571884d082b
Oery <oery.dev@gmail.com>
2026-09-05 00:38:08 +0200

    feat: add support for brightness osd

--- modules/aspects/user/hyprland.nix
+++ modules/aspects/user/hyprland.nix
@@ -204,8 +204,8 @@
           bindle = [
             ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
             ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-            ", XF86MonBrightnessUp, exec, brightnessctl set \"12.75+\""
-            ", XF86MonBrightnessDown, exec, brightnessctl set \"12.75-\""
+            ", XF86MonBrightnessUp, exec, brightnessctl set \"12.75+\" && qs -c oery ipc call brightness display"
+            ", XF86MonBrightnessDown, exec, brightnessctl set \"12.75-\" && qs -c oery ipc call brightness display"
           ];
 
           bindm = [
